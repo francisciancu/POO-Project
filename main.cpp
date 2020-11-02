24a8aa09dd73bcd410de22d5fc70fdf0545c7a46
BuildTools <unconfigured@null.spigotmc.org>
2020-11-02 11:43:55 +0200

    little work

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+#include <locale>
+#include <algorithm>
 
 
 using namespace std;
@@ -8,7 +11,31 @@ private:
     int Nr_RAM;
     int Frecventa_Procesor;
     int Nr_VRAM;
+    string Nume_PC;
+public:
+    //constructor de initializare
+    Calculator(int Nr_RAM=0,int Frecventa_Procesor=0,int Nr_VRAM=0,string Nume_PC="0"){
+
+        this->Nr_RAM=Nr_RAM;
+        this->Frecventa_Procesor=Frecventa_Procesor;
+        this->Nr_VRAM=Nr_VRAM;
+        this->Nume_PC=Nume_PC;
+    }
+    void This_PC (int Nr_RAM,int Frecventa_Procesor,int Nr_VRAM,string Nume_PC){
+
+        this->Nr_RAM=Nr_RAM;
+        this->Frecventa_Procesor=Frecventa_Procesor;
+        this->Nr_VRAM=Nr_VRAM;
+        this->Nume_PC=Nume_PC;
+    }
+    //Destructor
+    ~Calculator(){
+        cout<<"This PC( "<<Nume_PC<<" ) Burned Down( to much power :O )";
+    }
+
 };
+
+
 class Monitor{
 private:
     int Nr_Pixeli_Inaltime;
@@ -47,7 +74,29 @@ public:
         os << "Rezolutia este : " <<obj.Rezolutie<< "\n";
         return os;
     }
+    void Distrugere_Monitor(string Raspuns,int Validare_Raspuns){
 
+        for_each(Raspuns.begin(), Raspuns.end(), [](char & c){
+            c =tolower(c);
+        });
+        if(Raspuns == "da")
+        {
+            delete this; // test destructor
+            int Validare_Raspuns=1;
+        }
+
+        else if (Raspuns == "nu")
+        {
+            cout<<"OK! But don't forget about it !"<<"\n";
+            int Validare_Raspuns=1;
+        }
+        else
+        {cout<<"Raspuns Invalid !"<<"\n"<<"Introduce-ti un raspuns corect. Da/Nu"<<"\n";
+        cin>>Raspuns;
+        this->Distrugere_Monitor(Raspuns,Validare_Raspuns);}
+
+
+    }
 
 
 
@@ -65,8 +114,31 @@ public:
     int rezolutieH;
 };
 class MMORPG : public Joc{
+    public:
     string Tip_Joc="MMORPG";
+    MMORPG(){
+        this->Multiplayer= true;
+
+
+    }
+    int Max_players;
+    int Ore_Gameplay;
+    int Nr_Servers;
+    int Nr_Harti;
+    string Dificultate;
+
 };
+//Functie Creeare PC
+
+void Creeare_Pc (Calculator *PC){
+
+
+
+
+}
+
+
+//Functie creeare montior
 
 void Creeare_Monitor(Monitor *rez){
     int rez_mon;
@@ -77,13 +149,9 @@ void Creeare_Monitor(Monitor *rez){
     cout<<" 3840 x 2160 - 4"<<endl;
     cout<<" 7680 x 4320 - 5"<<endl;
     cout<<" Introduce-ti numarul corespunzator rezolutiei dumneavoastra : ";
-    cin>>rez_mon;
-if (rez_mon<1 || rez_mon>5)
-{
-    cout<<"Alegere incorecta !";
-    cin>>rez_mon;
 
-}
+
+    do{ cin>>rez_mon;
     switch (rez_mon) {
         case 1:
             rez->Aspect_rezolutie(720,1280,"720p");
@@ -100,18 +168,52 @@ if (rez_mon<1 || rez_mon>5)
         case 5:
             rez->Aspect_rezolutie(4320,7680,"4320p/8K");
             break;
+        default:
+        {cout<<"Alegere incorecta ! Introduce-ti un raspuns corect."<<"\n";
+            cout<<" Care este rezolutia monitorului dumneavoastra?"<<endl;
+            cout<<" 1280 x 720  - 1"<<endl;
+            cout<<" 1920 x 1080 - 2"<<endl;
+            cout<<" 2560 x 1440 - 3"<<endl;
+            cout<<" 3840 x 2160 - 4"<<endl;
+            cout<<" 7680 x 4320 - 5"<<endl;
+            cout<<" Introduce-ti numarul corespunzator rezolutiei dumneavoastra : ";
 
+        }
     }
+    }while( rez_mon<1 || rez_mon>5 );
+
+
 }
 
+//Functie Creeare Joc
+/*void Creeare_Joc(){
 
-//Functie creeare montir
+
+
+
+}*/
+
+
+
 
 int main() {
     Monitor *rez=new Monitor();
     Creeare_Monitor(rez);
     cout<<*rez;
-    delete rez; // test destructor
+    cout<<"\n"<<"Doresti sa distrugi acest monitor?  Da/Nu"<<"\n";
+    string Raspuns;
+    cin>>Raspuns;
+    int Validare_Raspuns=0;
+    rez->Distrugere_Monitor(Raspuns,Validare_Raspuns);
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
