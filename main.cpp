da0f06e5761da16e4cd2840802bc3c51c6ec3076
BuildTools <unconfigured@null.spigotmc.org>
2020-11-08 20:09:53 +0200

    little work

--- main.cpp
+++ main.cpp
@@ -74,37 +74,37 @@ public:
         os << "Rezolutia este : " <<obj.Rezolutie<< "\n";
         return os;
     }
-    void Distrugere_Monitor(string Raspuns,int Validare_Raspuns){
+   //void Distrugere_Monitor(string Raspuns,int Validare_Raspuns){
+//
+  //      for_each(Raspuns.begin(), Raspuns.end(), [](char & c){
+   //         c =tolower(c);
+   //     });
+   //     Raspuns.erase(remove(Raspuns.begin(), Raspuns.end(), ' '), Raspuns.end());
+  //      if(Raspuns == "da")
+   //     {
+   //         delete this; // test destructor
+   //         int Validare_Raspuns=1;
+   //     }
+//
+ //       else if (Raspuns == "nu")
+ //       {
+ //           cout<<"OK! But don't forget about it !"<<"\n";
+  //          int Validare_Raspuns=1;
+  //      }
+  //      else
+   //     {cout<<"Raspuns Invalid !"<<"\n"<<"Introduce-ti un raspuns corect. Da/Nu"<<"\n";
+  //          cin>>Raspuns;
+  //          this->Distrugere_Monitor(Raspuns,Validare_Raspuns);}
 
-        for_each(Raspuns.begin(), Raspuns.end(), [](char & c){
-            c =tolower(c);
-        });
-        Raspuns.erase(remove(Raspuns.begin(), Raspuns.end(), ' '), Raspuns.end());
-        if(Raspuns == "da")
-        {
-            delete this; // test destructor
-            int Validare_Raspuns=1;
-        }
 
-        else if (Raspuns == "nu")
-        {
-            cout<<"OK! But don't forget about it !"<<"\n";
-            int Validare_Raspuns=1;
-        }
-        else
-        {cout<<"Raspuns Invalid !"<<"\n"<<"Introduce-ti un raspuns corect. Da/Nu"<<"\n";
-            cin>>Raspuns;
-            this->Distrugere_Monitor(Raspuns,Validare_Raspuns);}
-
-
-    }
+ //   }
 
 
 
 };
 
 class Joc{
-public:
+protected:
     string Nume;
     int Pret;
     int RAM_Minim;
@@ -115,7 +115,7 @@ public:
     int rezolutieH;
 };
 class MMORPG : public Joc{
-public:
+protected:
     string Tip_Joc="MMORPG";
     MMORPG(){
         this->Multiplayer= true;
@@ -132,12 +132,73 @@ public:
 //Functie Creare PC
 
 void Creare_Pc (Calculator *PC){
+cout<<" Introduce-ti numarul de GB de RAM dorit ( Numar Maxim 64 ) : "<<'\n';
+string Nr_RAM_Introdus;
+getline(cin,Nr_RAM_Introdus);
+int Nr_RAM=stoi(Nr_RAM_Introdus);
+int validare1=1;
+while(validare1==1){
+if(Nr_RAM>64 || Nr_RAM<0)
+{
+        cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
+        getline(cin,Nr_RAM_Introdus);
+        Nr_RAM=stoi(Nr_RAM_Introdus);
+        validare1=1;}
+    else
+    {
+        validare1=0;
+    }
+}
 
+string Nr_VRAM_Introdus;
+cout <<"Introduce-ti numarul de Gb de VRAM dorit ( Numar Maxim 16 ) : "<< "\n";
+getline(cin,Nr_VRAM_Introdus);
+int Nr_VRAM=stoi(Nr_VRAM_Introdus);
+int validare2=1;
+while(validare2==1){
+    if(Nr_VRAM>16 || Nr_VRAM<0)
+    {
+        cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
+        getline(cin,Nr_VRAM_Introdus);
+        Nr_VRAM=stoi(Nr_VRAM_Introdus);
+        validare2=1;}
+        else
+            validare2=0;
+    }
 
+string Frecventa_Procesor_Introdusa;
+cout<<"Introduce-ti numarul de GHz ai procesorului ( Numar Maxim 6 ) : "<< "\n";
+getline(cin,Frecventa_Procesor_Introdusa);
+int Frecventa_Procesor=stoi(Frecventa_Procesor_Introdusa);
+    int validare3=1;
+    while(validare3==1){
+        if(Frecventa_Procesor>6 || Frecventa_Procesor<0)
+        {
+            cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
+            getline(cin,Frecventa_Procesor_Introdusa);
+            Frecventa_Procesor=stoi(Frecventa_Procesor_Introdusa);
+            validare3=1;}
+            else
+            validare3=0;
+        }
+
+    string Nume_PC;
+    cout<<" Introduce-ti numele calculatorului "<<'\n';
+    getline(cin,Nume_PC);
+    if(validare1==validare2==validare3==0){
+        cout<<"Numele calculatorului este : "<<Nume_PC<<'\n';
+        cout<<"Numar Gb RAM : "<<Nr_RAM<<'\n';
+        cout<<"Numar Gb VRAM : "<<Nr_VRAM<<'\n';
+        cout<<"Numar de Ghz ai procesorului : "<<Frecventa_Procesor<<'\n';
+
+    }
 
 
 }
 
+
+
+///std make uique
 //Functie Creare Montior
 
 void Creare_Monitor(Monitor *rez){
@@ -159,7 +220,7 @@ void Creare_Monitor(Monitor *rez){
         rez_mon.erase(remove(rez_mon.begin(), rez_mon.end(), ' '), rez_mon.end());
     }
     char a = rez_mon[0];
-
+//stoi
     switch (a) {
         case '1':
             rez->Aspect_rezolutie(720,1280,"720p");
@@ -196,11 +257,13 @@ int main() {
     Monitor *rez=new Monitor();
     Creare_Monitor(rez);
     cout<<*rez;
-    cout<<"\n"<<"Doresti sa distrugi acest monitor?  Da/Nu"<<"\n";
-    string Raspuns;
-    getline(cin, Raspuns);
-    int Validare_Raspuns=0;
-    rez->Distrugere_Monitor(Raspuns,Validare_Raspuns);
+    //cout<<"\n"<<"Doresti sa distrugi acest monitor?  Da/Nu"<<"\n";
+    //string Raspuns;
+    //getline(cin, Raspuns);
+    //int Validare_Raspuns=0;
+   //rez->Distrugere_Monitor(Raspuns,Validare_Raspuns);
+   Calculator *PC=new Calculator();
+   Creare_Pc(PC);
 
 
     return 0;
