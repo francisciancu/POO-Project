c0a34ce396780637b4413d6c235445fef0016261
BuildTools <unconfigured@null.spigotmc.org>
2020-12-06 18:56:14 +0200

    nah

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-class Calculator{
+class Calculator {
 private:
     int Nr_RAM;
     int Frecventa_Procesor;
@@ -13,96 +13,101 @@ private:
     string Nume_PC;
 public:
     //constructor de initializare
-    Calculator(int Nr_RAM=0,int Frecventa_Procesor=0,int Nr_VRAM=0,string Nume_PC="0"){
+    Calculator(int Nr_RAM = 0, int Frecventa_Procesor = 0, int Nr_VRAM = 0, string Nume_PC = "0") {
 
-        this->Nr_RAM=Nr_RAM;
-        this->Frecventa_Procesor=Frecventa_Procesor;
-        this->Nr_VRAM=Nr_VRAM;
-        this->Nume_PC=Nume_PC;
+        this->Nr_RAM = Nr_RAM;
+        this->Frecventa_Procesor = Frecventa_Procesor;
+        this->Nr_VRAM = Nr_VRAM;
+        this->Nume_PC = Nume_PC;
     }
-    void This_PC (int Nr_RAM,int Frecventa_Procesor,int Nr_VRAM,string Nume_PC){
 
-        this->Nr_RAM=Nr_RAM;
-        this->Frecventa_Procesor=Frecventa_Procesor;
-        this->Nr_VRAM=Nr_VRAM;
-        this->Nume_PC=Nume_PC;
+    void This_PC(int Nr_RAM, int Frecventa_Procesor, int Nr_VRAM, string Nume_PC) {
+
+        this->Nr_RAM = Nr_RAM;
+        this->Frecventa_Procesor = Frecventa_Procesor;
+        this->Nr_VRAM = Nr_VRAM;
+        this->Nume_PC = Nume_PC;
     }
+
     //Destructor
-    ~Calculator(){
-        cout<<"This PC( "<<Nume_PC<<" ) Burned Down( to much power :O )";
+    ~Calculator() {
+        cout << "This PC( " << Nume_PC << " ) Burned Down( to much power :O )";
     }
 
 };
 
 
-class Monitor{
+class Monitor {
 private:
     int Nr_Pixeli_Inaltime;
     int Nr_Pixeli_Latime;
     string Rezolutie;
 public:
     //contructor- initializare
-    Monitor(int Nr_Pixeli_Inaltime=0,int Nr_Pixeli_Latime=0,string Rezolutie="0"){
-        this -> Nr_Pixeli_Inaltime = Nr_Pixeli_Inaltime;
-        this -> Nr_Pixeli_Latime = Nr_Pixeli_Latime;
-        this -> Rezolutie = Rezolutie;
+    Monitor(int Nr_Pixeli_Inaltime = 0, int Nr_Pixeli_Latime = 0, string Rezolutie = "0") {
+        this->Nr_Pixeli_Inaltime = Nr_Pixeli_Inaltime;
+        this->Nr_Pixeli_Latime = Nr_Pixeli_Latime;
+        this->Rezolutie = Rezolutie;
     }//Operator=
 
-    Monitor &operator= (const Monitor &obj ){
-        Nr_Pixeli_Inaltime=obj.Nr_Pixeli_Inaltime;
-        Nr_Pixeli_Latime=obj.Nr_Pixeli_Latime;
-        Rezolutie=obj.Rezolutie;
+    Monitor &operator=(const Monitor &obj) {
+        Nr_Pixeli_Inaltime = obj.Nr_Pixeli_Inaltime;
+        Nr_Pixeli_Latime = obj.Nr_Pixeli_Latime;
+        Rezolutie = obj.Rezolutie;
         return *this;
     }
-    void Aspect_rezolutie(int Nr_Pixeli_Inaltime,int Nr_Pixeli_Latime,string Rezolutie){
-        this -> Nr_Pixeli_Inaltime = Nr_Pixeli_Inaltime;
-        this -> Nr_Pixeli_Latime = Nr_Pixeli_Latime;
-        this -> Rezolutie = Rezolutie;
+
+    void Aspect_rezolutie(int Nr_Pixeli_Inaltime, int Nr_Pixeli_Latime, string Rezolutie) {
+        this->Nr_Pixeli_Inaltime = Nr_Pixeli_Inaltime;
+        this->Nr_Pixeli_Latime = Nr_Pixeli_Latime;
+        this->Rezolutie = Rezolutie;
     }
+
     //Destructor
-    ~Monitor(){
-        cout<<"Monitor Goes Brrr( dieded :o )";
+    ~Monitor() {
+        cout << "Monitor Goes Brrr( dieded :o )";
     }
+
     // Constructor Copiere
-    Monitor (const Monitor & obj) : Monitor (obj.Nr_Pixeli_Inaltime,obj.Nr_Pixeli_Latime,obj.Rezolutie){
+    Monitor(const Monitor &obj) : Monitor(obj.Nr_Pixeli_Inaltime, obj.Nr_Pixeli_Latime, obj.Rezolutie) {
 
     }
+
     //Operator <<
-    friend ostream& operator<<(ostream& os, const Monitor& obj)
-    {
-        os << "Rezolutia este : " <<obj.Rezolutie<< "\n";
+    friend ostream &operator<<(ostream &os, const Monitor &obj) {
+        os << "Rezolutia este : " << obj.Rezolutie << "\n";
         return os;
     }
-   //void Distrugere_Monitor(string Raspuns,int Validare_Raspuns){
+    //void Distrugere_Monitor(string Raspuns,int Validare_Raspuns){
 //
-  //      for_each(Raspuns.begin(), Raspuns.end(), [](char & c){
-   //         c =tolower(c);
-   //     });
-   //     Raspuns.erase(remove(Raspuns.begin(), Raspuns.end(), ' '), Raspuns.end());
-  //      if(Raspuns == "da")
-   //     {
-   //         delete this; // test destructor
-   //         int Validare_Raspuns=1;
-   //     }
+    //      for_each(Raspuns.begin(), Raspuns.end(), [](char & c){
+    //         c =tolower(c);
+    //     });
+    //     Raspuns.erase(remove(Raspuns.begin(), Raspuns.end(), ' '), Raspuns.end());
+    //      if(Raspuns == "da")
+    //     {
+    //         delete this; // test destructor
+    //         int Validare_Raspuns=1;
+    //     }
 //
- //       else if (Raspuns == "nu")
- //       {
- //           cout<<"OK! But don't forget about it !"<<"\n";
-  //          int Validare_Raspuns=1;
-  //      }
-  //      else
-   //     {cout<<"Raspuns Invalid !"<<"\n"<<"Introduce-ti un raspuns corect. Da/Nu"<<"\n";
-  //          cin>>Raspuns;
-  //          this->Distrugere_Monitor(Raspuns,Validare_Raspuns);}
+    //       else if (Raspuns == "nu")
+    //       {
+    //           cout<<"OK! But don't forget about it !"<<"\n";
+    //          int Validare_Raspuns=1;
+    //      }
+    //      else
+    //     {cout<<"Raspuns Invalid !"<<"\n"<<"Introduce-ti un raspuns corect. Da/Nu"<<"\n";
+    //          cin>>Raspuns;
+    //          this->Distrugere_Monitor(Raspuns,Validare_Raspuns);}
 
 
- //   }
+    //   }
 
 
 
 };
 
-class Joc{
+class Joc {
 protected:
     string Nume;
     int Pret;
@@ -123,12 +128,14 @@ protected:
     }*/
 
 };
-class MMORPG : protected Joc{
+
+class MMORPG : protected Joc {
 protected:
-    MMORPG(){
-        this->Multiplayer= true;
-        this->Tip_Joc="MMORPG";
+    MMORPG() {
+        this->Multiplayer = true;
+        this->Tip_Joc = "MMORPG";
     }
+
     int Max_players;
     int Ore_Gameplay;
     int Nr_Servers;
@@ -137,12 +144,13 @@ protected:
 
 };
 
-class Shooter : protected Joc{
+class Shooter : protected Joc {
 protected:
-    Shooter(){
-        this->Multiplayer= true;
-        this->Tip_Joc="Shooter";
+    Shooter() {
+        this->Multiplayer = true;
+        this->Tip_Joc = "Shooter";
     }
+
     int Max_players;
     int Nr_Servers;
     int Nr_Harti;
@@ -150,15 +158,16 @@ protected:
 
 };
 
-class SingleplayerGame : protected Joc{
+class SingleplayerGame : protected Joc {
 protected:
-    SingleplayerGame(){
-        this->Multiplayer=false;
-        this->Tip_Joc="Singleplayer";
+    SingleplayerGame() {
+        this->Multiplayer = false;
+        this->Tip_Joc = "Singleplayer";
     }
+
     int Ore_Gameplay;
     int Nr_Dificultati;
-    vector<string>Dificultati;
+    vector<string> Dificultati;
     string Tip_Joc;
 };
 
@@ -168,65 +177,61 @@ protected:
 
 //Functie Creare PC
 
-void Creare_Pc (Calculator *PC){
-cout<<" Introduce-ti numarul de GB de RAM dorit ( Numar Maxim 64 ) : "<<'\n';
-string Nr_RAM_Introdus;
-getline(cin,Nr_RAM_Introdus);
-int Nr_RAM=stoi(Nr_RAM_Introdus);
-int validare1=1;
-while(validare1==1){
-if(Nr_RAM>64 || Nr_RAM<0)
-{
-        cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
-        getline(cin,Nr_RAM_Introdus);
-        Nr_RAM=stoi(Nr_RAM_Introdus);
-        validare1=1;}
-    else
-    {
-        validare1=0;
-    }
-}
-
-string Nr_VRAM_Introdus;
-cout <<"Introduce-ti numarul de Gb de VRAM dorit ( Numar Maxim 16 ) : "<< "\n";
-getline(cin,Nr_VRAM_Introdus);
-int Nr_VRAM=stoi(Nr_VRAM_Introdus);
-int validare2=1;
-while(validare2==1){
-    if(Nr_VRAM>16 || Nr_VRAM<0)
-    {
-        cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
-        getline(cin,Nr_VRAM_Introdus);
-        Nr_VRAM=stoi(Nr_VRAM_Introdus);
-        validare2=1;}
-        else
-            validare2=0;
-    }
-
-string Frecventa_Procesor_Introdusa;
-cout<<"Introduce-ti numarul de GHz ai procesorului ( Numar Maxim 6 ) : "<< "\n";
-getline(cin,Frecventa_Procesor_Introdusa);
-int Frecventa_Procesor=stoi(Frecventa_Procesor_Introdusa);
-    int validare3=1;
-    while(validare3==1){
-        if(Frecventa_Procesor>6 || Frecventa_Procesor<0)
-        {
+void Creare_Pc(Calculator *PC) {
+    cout << " Introduce-ti numarul de GB de RAM dorit ( Numar Maxim 64 ) : " << '\n';
+    string Nr_RAM_Introdus;
+    getline(cin, Nr_RAM_Introdus);
+    int Nr_RAM = stoi(Nr_RAM_Introdus);
+    int validare1 = 1;
+    while (validare1 == 1) {
+        if (Nr_RAM > 64 || Nr_RAM < 0) {
             cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
-            getline(cin,Frecventa_Procesor_Introdusa);
-            Frecventa_Procesor=stoi(Frecventa_Procesor_Introdusa);
-            validare3=1;}
-            else
-            validare3=0;
+            getline(cin, Nr_RAM_Introdus);
+            Nr_RAM = stoi(Nr_RAM_Introdus);
+            validare1 = 1;
+        } else {
+            validare1 = 0;
         }
+    }
+
+    string Nr_VRAM_Introdus;
+    cout << "Introduce-ti numarul de Gb de VRAM dorit ( Numar Maxim 16 ) : " << "\n";
+    getline(cin, Nr_VRAM_Introdus);
+    int Nr_VRAM = stoi(Nr_VRAM_Introdus);
+    int validare2 = 1;
+    while (validare2 == 1) {
+        if (Nr_VRAM > 16 || Nr_VRAM < 0) {
+            cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
+            getline(cin, Nr_VRAM_Introdus);
+            Nr_VRAM = stoi(Nr_VRAM_Introdus);
+            validare2 = 1;
+        } else
+            validare2 = 0;
+    }
+
+    string Frecventa_Procesor_Introdusa;
+    cout << "Introduce-ti numarul de GHz ai procesorului ( Numar Maxim 6 ) : " << "\n";
+    getline(cin, Frecventa_Procesor_Introdusa);
+    int Frecventa_Procesor = stoi(Frecventa_Procesor_Introdusa);
+    int validare3 = 1;
+    while (validare3 == 1) {
+        if (Frecventa_Procesor > 6 || Frecventa_Procesor < 0) {
+            cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
+            getline(cin, Frecventa_Procesor_Introdusa);
+            Frecventa_Procesor = stoi(Frecventa_Procesor_Introdusa);
+            validare3 = 1;
+        } else
+            validare3 = 0;
+    }
 
     string Nume_PC;
-    cout<<" Introduce-ti numele calculatorului "<<'\n';
-    getline(cin,Nume_PC);
-    if(validare1==validare2==validare3==0){
-        cout<<"Numele calculatorului este : "<<Nume_PC<<'\n';
-        cout<<"Numar Gb RAM : "<<Nr_RAM<<'\n';
-        cout<<"Numar Gb VRAM : "<<Nr_VRAM<<'\n';
-        cout<<"Numar de Ghz ai procesorului : "<<Frecventa_Procesor<<'\n';
+    cout << " Introduce-ti numele calculatorului " << '\n';
+    getline(cin, Nume_PC);
+    if (validare1 == validare2 == validare3 == 0) {
+        cout << "Numele calculatorului este : " << Nume_PC << '\n';
+        cout << "Numar Gb RAM : " << Nr_RAM << '\n';
+        cout << "Numar Gb VRAM : " << Nr_VRAM << '\n';
+        cout << "Numar de Ghz ai procesorului : " << Frecventa_Procesor << '\n';
 
     }
 
@@ -238,20 +243,20 @@ int Frecventa_Procesor=stoi(Frecventa_Procesor_Introdusa);
 
 //Functie Creare Montior
 
-void Creare_Monitor(Monitor *rez){
+void Creare_Monitor(Monitor *rez) {
 
-    cout<<" Care este rezolutia monitorului dumneavoastra?"<<endl;
-    cout<<" 1280 x 720  - 1"<<endl;
-    cout<<" 1920 x 1080 - 2"<<endl;
-    cout<<" 2560 x 1440 - 3"<<endl;
-    cout<<" 3840 x 2160 - 4"<<endl;
-    cout<<" 7680 x 4320 - 5"<<endl;
-    cout<<" Introduce-ti numarul corespunzator rezolutiei dumneavoastra : ";
+    cout << " Care este rezolutia monitorului dumneavoastra?" << endl;
+    cout << " 1280 x 720  - 1" << endl;
+    cout << " 1920 x 1080 - 2" << endl;
+    cout << " 2560 x 1440 - 3" << endl;
+    cout << " 3840 x 2160 - 4" << endl;
+    cout << " 7680 x 4320 - 5" << endl;
+    cout << " Introduce-ti numarul corespunzator rezolutiei dumneavoastra : ";
 
     string rez_mon;
     getline(cin, rez_mon);
     rez_mon.erase(remove(rez_mon.begin(), rez_mon.end(), ' '), rez_mon.end());
-    while(rez_mon.size() > 1 || (rez_mon[0] < '1' || rez_mon[0] > '5')) {
+    while (rez_mon.size() > 1 || (rez_mon[0] < '1' || rez_mon[0] > '5')) {
         cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
         getline(cin, rez_mon);
         rez_mon.erase(remove(rez_mon.begin(), rez_mon.end(), ' '), rez_mon.end());
@@ -259,19 +264,19 @@ void Creare_Monitor(Monitor *rez){
     char a = rez_mon[0];
     switch (a) {
         case '1':
-            rez->Aspect_rezolutie(720,1280,"720p");
+            rez->Aspect_rezolutie(720, 1280, "720p");
             break;
         case '2':
-            rez->Aspect_rezolutie(1080,1920,"1080p");
+            rez->Aspect_rezolutie(1080, 1920, "1080p");
             break;
         case '3':
-            rez->Aspect_rezolutie(1440,2560,"1440p");
+            rez->Aspect_rezolutie(1440, 2560, "1440p");
             break;
         case '4':
-            rez->Aspect_rezolutie(2160,3840,"2160p/4K");
+            rez->Aspect_rezolutie(2160, 3840, "2160p/4K");
             break;
         case '5':
-            rez->Aspect_rezolutie(4320,7680,"4320p/8K");
+            rez->Aspect_rezolutie(4320, 7680, "4320p/8K");
             break;
         default:
             break;
@@ -279,51 +284,46 @@ void Creare_Monitor(Monitor *rez){
 }
 
 int main() {
-    cout<<"Doriti sa creati un nou monitor ? "<<'\n';
-    cout<<"0-NU"<<'\n';
-    cout<<"1-DA"<<'\n';
+    cout << "Doriti sa creati un nou monitor ? " << '\n';
+    cout << "0-NU" << '\n';
+    cout << "1-DA" << '\n';
     string ras1;
-    getline(cin,ras1);
+    getline(cin, ras1);
     ras1.erase(remove(ras1.begin(), ras1.end(), ' '), ras1.end());
-    while(ras1.size() > 1 || (ras1[0] < '0' || ras1[0] > '1')) {
+    while (ras1.size() > 1 || (ras1[0] < '0' || ras1[0] > '1')) {
         cout << "Alegere incorecta ! Introduce-ti un raspuns corect." << "\n";
         getline(cin, ras1);
         ras1.erase(remove(ras1.begin(), ras1.end(), ' '), ras1.end());
     }
-    int ans1=stoi(ras1);
-    if(ans1 == 1)
-    {
-        Monitor *rez=new Monitor();
+    int ans1 = stoi(ras1);
+    if (ans1 == 1) {
+        Monitor *rez = new Monitor();
         Creare_Monitor(rez);
-        cout<<*rez;
-    }
-    else {
-        cout<<"O zi buna si multumim pentru folosirea aplicatiei noastre! :) ";
-       return 0;
+        cout << *rez;
+    } else {
+        cout << "O zi buna si multumim pentru folosirea aplicatiei noastre! :) ";
+        return 0;
     }
 
     //cout<<"\n"<<"Doresti sa distrugi acest monitor?  Da/Nu"<<"\n";
     //string Raspuns;
     //getline(cin, Raspuns);
     //int Validare_Raspuns=0;
-   //rez->Distrugere_Monitor(Raspuns,Validare_Raspuns);
+    //rez->Distrugere_Monitor(Raspuns,Validare_Raspuns);
 
-    cout<<"Doriti sa creati un nou calculator ? "<<'\n';
-    cout<<"0-NU"<<'\n';
-    cout<<"1-DA"<<'\n';
+    cout << "Doriti sa creati un nou calculator ? " << '\n';
+    cout << "0-NU" << '\n';
+    cout << "1-DA" << '\n';
     string ras2;
-    getline(cin,ras2);
+    getline(cin, ras2);
     ras2.erase(remove(ras2.begin(), ras2.end(), ' '), ras2.end());
-    int ans2=stoi(ras2);
-    if(ans2==1)
-    {
-        Calculator *PC=new Calculator();
+    int ans2 = stoi(ras2);
+    if (ans2 == 1) {
+        Calculator *PC = new Calculator();
         Creare_Pc(PC);
-    }
-    else {
+    } else {
         return 0;
     }
-
 
 
     return 0;
